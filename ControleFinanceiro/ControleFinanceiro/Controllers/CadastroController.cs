c75442a2fac0eaf81812399c80a8476c9659c716
leonardoaguiar <leonardo.r.aguiar@gmail.com>
2012-03-29 17:24:06 -0300

    Cadastro.

---
+++ ControleFinanceiro/ControleFinanceiro/Controllers/CadastroController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleFinanceiro.Controllers
+{
+    public class CadastroController : Controller
+    {
+        //
+        // GET: /Cadastro/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Cadastro/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Cadastro/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Cadastro/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Cadastro/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Cadastro/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Cadastro/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Cadastro/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ControleFinanceiro/ControleFinanceiro/Models/CadastroModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleFinanceiro.Models
+{
+    public class CadastroModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Nome {get; set;}
+
+        [Required]
+        [StringLength(50)]
+        public string Sobrenome {get; set;}
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name="Código")]
+        public int Codigo {get; set;}
+
+        [StringLength(150)]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Data { get; set; }
+    }
+}
